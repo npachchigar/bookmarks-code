898767edc33913d3784221e0865d590bd5dc29ee
Nirali Pachchigar <nnpachchigar1@buffs.wtamu.edu>
2021-10-13 21:26:05 -0500

    JQuery

--- images/views.py
+++ images/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
 from .forms import ImageCreateForm
+from .models import Image
 
 @login_required
 def image_create(request):
@@ -26,6 +28,13 @@ def image_create(request):
         form = ImageCreateForm(data=request.GET)
 
     return render(request,
-                 'images/image/create.html',
-                 {'section': 'images',
-                  'form': form})
+                  'images/image/create.html',
+                  {'section': 'images',
+                   'form': form})    
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+                  'images/image/detail.html',
+                  {'section': 'images',
+                   'image': image})
